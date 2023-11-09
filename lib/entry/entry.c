412825da3a1b33b1930f6d1a03776434392d12e5
jtainer <jonathantainer@yahoo.com>
2023-11-09 03:36:59 -0500

    Basic example reading from IMU

--- lib/entry/entry.c
+++ lib/entry/entry.c
@@ -8,17 +8,22 @@
 
 #include "mux.h"
 #include "imu.h"
+#include "mpu6050.h"
 #include "main.h"
 #include <stdio.h>
+
+#define IMU_CHANNELS 16
+MPU6050_t imu[IMU_CHANNELS] = { 0 };
 
 void setup(void) {
 	I2C_MUX_Init();
 	I2C_MUX_Select(0);
-	IMU_Init();
+	while (MPU6050_Init(&hi2c1) == 1);
 }
 
 void loop(void) {
-	uint8_t whoami = IMU_ReadRegister(117);
-	printf("%x\n", (unsigned int) whoami);
-	HAL_Delay(1000);
+	MPU6050_Read_All(&hi2c1, &imu[0]);
+	printf("Ax = %d\tAy = %d\t Az = %d\r\n",
+		imu[0].Ax, imu[0].Ay, imu[0].Az);
+	HAL_Delay(100);
 }
