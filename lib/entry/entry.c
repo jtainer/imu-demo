9e566f382f3bed494cb26d7a1d884671887dab53
jtainer <jonathantainer@yahoo.com>
2023-11-16 11:31:32 -0500

    Disable OLED display code for now since my prototype board does not have one

--- lib/entry/entry.c
+++ lib/entry/entry.c
@@ -19,7 +19,7 @@ void setup(void) {
 	I2C_MUX_Init();
 	I2C_MUX_Select(0);
 	while (MPU6050_Init(&hi2c1) == 1);
-	ssd1306_Init();
+//	ssd1306_Init();
 }
 
 void loop(void) {
@@ -29,6 +29,6 @@ void loop(void) {
 	printf("Ang.x = %d\t\tAng.y = %d\t\tAcc.x = %d\t\tAcc.y = %d\t\tAcc.z = %d\r\n",
 		(int)imu[0].KalmanAngleX, (int)imu[0].KalmanAngleY,
 		(int)imu[0].Accel_X_RAW, (int)imu[0].Accel_Y_RAW, (int)imu[0].Accel_Z_RAW);
-	ssd1306_Fill(screen_on ? White : Black);
+//	ssd1306_Fill(screen_on ? White : Black);
 	HAL_Delay(100);
 }
