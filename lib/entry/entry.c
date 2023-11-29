0c0324f98698f4dd08968e6f141c04d4178a8773
jtainer <jonathantainer@yahoo.com>
2023-11-29 14:14:59 -0500

    Print quaternion rotation instead of euler angles

--- lib/entry/entry.c
+++ lib/entry/entry.c
@@ -19,6 +19,7 @@ void setup(void) {
 	I2C_MUX_Init();
 	I2C_MUX_Select(0);
 	while (MPU6050_Init(&hi2c1) == 1);
+	imu[0].rotation.w=1;
 //	ssd1306_Init();
 }
 
@@ -26,9 +27,10 @@ void loop(void) {
 	static int screen_on = 0;
 	screen_on = !screen_on;
 	MPU6050_Read_All(&hi2c1, &imu[0]);
-	printf("w = %d\t\tx = %d\t\ty = %d\t\tz = %d\n",
-		(int)imu[0].rotation.w, (int)imu[0].rotation.x,
-		(int)imu[0].rotation.y, (int)imu[0].rotation.z);
-//	ssd1306_Fill(screen_on ? White : Black);
+	vec4 quat = imu[0].rotation;
+	printf("w = %d\t\tx = %d\t\ty = %d\t\tz = %d\r\n",
+		(int)(quat.w*1000), (int)(quat.x*1000),
+		(int)(quat.y*1000), (int)(quat.z*1000));
+	ssd1306_Fill(screen_on ? White : Black);
 	HAL_Delay(10);
 }
