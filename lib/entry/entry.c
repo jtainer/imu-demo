13bc27f0630331a69ba3c426d767e55dd4e81c53
jtainer <jonathantainer@yahoo.com>
2023-12-05 02:16:47 -0500

    Change printf formatting to output quaternion using floats

--- lib/entry/entry.c
+++ lib/entry/entry.c
@@ -20,17 +20,12 @@ void setup(void) {
 	I2C_MUX_Select(0);
 	while (MPU6050_Init(&hi2c1) == 1);
 	imu[0].rotation.w=1;
-//	ssd1306_Init();
 }
 
 void loop(void) {
-	static int screen_on = 0;
-	screen_on = !screen_on;
 	MPU6050_Read_All(&hi2c1, &imu[0]);
 	vec4 quat = imu[0].rotation;
-	printf("w = %d\t\tx = %d\t\ty = %d\t\tz = %d\r\n",
-		(int)(quat.w*1000), (int)(quat.x*1000),
-		(int)(quat.y*1000), (int)(quat.z*1000));
-	ssd1306_Fill(screen_on ? White : Black);
+	printf("w = %f\t\tx = %f\t\ty = %f\t\tz = %f\r\n",
+		quat.w, quat.x, quat.y, quat.z);
 	HAL_Delay(10);
 }
